556d53687c8984d61b5f9c6929b0c9a01af71766
Matheus <matheus.cbmdk@gmail.com>
2021-10-29 23:22:59 -0300

    Start project 4

--- Project 3 - Todo List - Initial/scripts.js
+++ Project 3 - Todo List - Initial/scripts.js
@@ -18,7 +18,13 @@ const todosApp = {
             }else{
                 alert("To-do text is required");
             }
+        },
+        storeTodos() {
+            localStorage.setItem("todos", JSON.stringify(this.todos));
         }
+    },
+    created() {
+        this.todos = localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : this.todos;
     }
 }
 
