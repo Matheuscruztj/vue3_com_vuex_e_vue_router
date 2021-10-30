1485ca9b68d45a71d01942457b31c60171aa818a
Matheus Santos <matheus.cbmdk@gmail.com>
2021-10-29 21:08:06 -0300

    Update 3

---
+++ Project 3 - Todo List - Initial/scripts.js
@@ -0,0 +1,25 @@
+const todosApp = {
+    data() {
+        return {
+            todos: [],
+            newTodo: {
+                done: false
+            }
+        }
+    },
+    methods: {
+        addTodo: function() {
+            if(this.newTodo.text){
+                this.todos.push(this.newTodo);
+                this.newTodo = {
+                    done: false
+                };
+                localStorage.setItem("todos", JSON.stringify(this.todos));
+            }else{
+                alert("To-do text is required");
+            }
+        }
+    }
+}
+
+Vue.createApp(todosApp).mount('#app');
