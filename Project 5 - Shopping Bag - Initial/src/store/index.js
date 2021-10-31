2284e8659e9c040e7b03c22c84ec423406d46323
Matheus Santos <matheus.cbmdk@gmail.com>
2021-10-31 02:39:01 -0300

    Project 5 done

--- Project 5 - Shopping Bag - Initial/src/store/index.js
+++ Project 5 - Shopping Bag - Initial/src/store/index.js
@@ -4,12 +4,25 @@ import axios from 'axios';
 
 export default createStore({
   state: {
-    products: []
+    products: [],
+    productsInBag: []
   },
   mutations: {
     loadProducts(state, products) {
       state.products = products;
-    }    
+    },
+    loadBag(state, products) {
+      state.productsInBag = products;
+    },
+    addToBag(state, product) {
+      state.productsInBag.push(product);
+      localStorage.setItem('productsInBag', JSON.stringify(state.productsInBag));
+    },
+    removeFromBag(state, productId) {
+      var updatedBag = state.productsInBag.filter(item => item.id != productId);
+      state.productsInBag = updatedBag;
+      localStorage.setItem('productsInBag', JSON.stringify(state.productsInBag));
+    }
   },
   actions: {
     loadProducts({ commit }) {
@@ -18,6 +31,19 @@ export default createStore({
       .then(response => {
         commit('loadProducts', response.data);
       })
+    },
+    loadBag({ commit }) {
+      if(localStorage.getItem('productsInBag')) {
+        commit('loadBag', JSON.parse(localStorage.getItem('productsInBag')));
+      }
+    },
+    addToBag({ commit }, product) {
+      commit('addToBag', product);
+    },
+    removeFromBag({ commit }, productId) {
+      if(confirm('Are you sure you want to remove the item from bag?')) {
+        commit('removeFromBag', productId);
+      }
     }
   },
   modules: {
